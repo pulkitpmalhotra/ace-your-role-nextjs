4a5d25f3011084fda259e47d8ec2e743ad74488e
pulkitpmalhotra <78749444+pulkitpmalhotra@users.noreply.github.com>
2025-07-27 10:41:20 -0400

    Delete api/_supabase.ts

--- api/_supabase.ts
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-  throw new Error('Missing Supabase environment variables');
-}
-module.exports = async function handler
