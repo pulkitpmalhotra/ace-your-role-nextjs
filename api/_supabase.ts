5234829f157c60dd9be5ea849859161dc4cac73f
pulkitpmalhotra <78749444+pulkitpmalhotra@users.noreply.github.com>
2025-07-27 10:24:25 -0400

    Update _supabase.ts

--- api/_supabase.ts
+++ api/_supabase.ts
@@ -1,11 +1,6 @@
-// api/_supabase.ts
-import { createClient } from '@supabase/supabase-js';
+const { createClient } = require('@supabase/supabase-js');
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Missing Supabase environment variables');
 }
-
-export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
+module.exports = async function handler
