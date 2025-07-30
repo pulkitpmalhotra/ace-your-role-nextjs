1745b11c13b9fc8fc03d12a1d8d88c2fc2ca8a37
pulkitpmalhotra <78749444+pulkitpmalhotra@users.noreply.github.com>
2025-07-30 09:26:29 -0400

    Delete config directory

--- config/google-cloud.js
+++
@@ -1,8 +0,0 @@
-// Google Cloud configuration
-const GOOGLE_CLOUD_CONFIG = {
-  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID || 'ace-your-role-speech',
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  location: 'global'
-};
-
-export default GOOGLE_CLOUD_CONFIG;
